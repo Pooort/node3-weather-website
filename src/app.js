4e62c605348be5f65ce1f1c752c15f6ffcbf1728
Oleg Port <oleg8angel@gmail.com>
2020-03-19 16:17:41 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const partialPath = path.join(__dirname, '../templates/partials')
 
-
+const port = process.env.PORT || 3000
 
 // Setup handlers
 app.set('views', path.join(__dirname, '../templates/views'))
@@ -90,6 +90,6 @@ app.get('*', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
